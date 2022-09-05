ddfe2cd858dbf9d7d31632b51a3c2afde3c68cb5
Gabriel <gspadovesi@gmail.com>
2022-09-05 12:19:41 -0300

    Project clean

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+//.render - Manda renderizar a aplicação no navegador 
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
