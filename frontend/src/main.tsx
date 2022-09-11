5a2c4fb3f9f6e93a509838d1447db45d3ce7ea06
Gabriel <gspadovesi@gmail.com>
2022-09-11 15:22:23 -0300

    Window React

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+window.React = React;
+
 //.render - Manda renderizar a aplicação no navegador 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
